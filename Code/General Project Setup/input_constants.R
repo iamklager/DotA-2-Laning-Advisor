437809e69afcd625440189cc5a1b311ffd1cdbb8
iamklager <gerhard.klager@gmail.com>
2024-05-27 12:06:53 +0200

    Fixed error resulting from merging.

--- Code/General Project Setup/input_constants.R
+++ Code/General Project Setup/input_constants.R
@@ -3,11 +3,8 @@
 
 
 ### Functional ----
-<<<<<<< HEAD
+
 c_Patch <- "7.36a"
-=======
-c_Patch <- "7.36"
->>>>>>> 33a715f039612a3db24eae42f8c4bea850b59bd8
 n_LVLRange <- 1:5
 c_UnselectableStats <- c("A", "LVL", "DMG_main", "ASM","AtkRngT", "AtkT_b", "HP_b", "MP_b", "MagRes_b", "SlwRes_b", "StsRes_b")
 c_CompStats <- c("ARM", "AtkRng", "AS", "DMG_avg", "HP", "HP_s", "MagRes", "MP", "MP_s", "MS", "SlwRes", "StsRes", "TR", "VS_d", "VS_n")
